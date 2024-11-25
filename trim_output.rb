2a3fbabfb017bcb27e06b11a96f6a999babd01ab
Justin Coyne <jcoyne@justincoyne.com>
2024-11-25 11:12:21 -0600

    Trim csv

--- trim_output.rb
+++ trim_output.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 CSV.open("fixed.csv", "wb") do |csv|
-  CSV.foreach('image_search_output_b.csv') do |row|
+  CSV.foreach('image_search_output_b.csv', liberal_parsing: true, row_sep: "\r\n") do |row|
     csv << [row[0], row[1], row[2]&.rstrip]
   end
 end
